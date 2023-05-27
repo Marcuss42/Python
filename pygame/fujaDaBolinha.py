caa8178ba7383ccab1983171de0abca164d0135f
Marcuss42 <105746798+Marcuss42@users.noreply.github.com>
2023-05-26 21:50:26 -0300

    Update fujaDaBolinha.py

--- pygame/fujaDaBolinha.py
+++ pygame/fujaDaBolinha.py
@@ -13,6 +13,36 @@ class Jogo:
             open("record.txt", "w").write("0")
         
         self.record = int(open("record.txt").read())
+        self.initialize_game()
+    
+    def getScreenSize(self):
+        tk = Tk()
+        width, height = tk.winfo_screenwidth(), tk.winfo_screenheight()
+        tk.destroy()
+        return  width, height
+        
+    def initialize_game(self):
+        pygame.init()
+        self.width, self.height = [size*0.7 for size in self.getScreenSize()]
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        
+        pygame.display.set_caption("Fuja Da Bolinha!")
+        
+        self.background = (0, 0, 0)
+        self.screen.fill(self.background)
+
+        self.pos = [20, 20]
+        self.color = (0, 0, 150)
+        self.velocidade = int(self.height/100) + 3
+        pygame.draw.circle(self.screen, self.color, self.pos, 10)
+
+        self.start = False
+        self.perdeu = False
+        
+        self.text_color = [255-i for i in self.background]
+        self.font = "monospace"
+        self.font_size = 30
+        self.score = 1
 
     def inRange(self, x, y, limit):
         equals = 0
@@ -25,8 +55,8 @@ class Jogo:
     def move(self, x, y):
         self.screen.fill(self.background)
         pygame.draw.circle(self.screen, self.color, self.pos, 10)
-        self.pos[0] += self.velo if self.pos[0] < x else -self.velo
-        self.pos[1] += self.velo if self.pos[1] < y else -self.velo
+        self.pos[0] += self.velocidade if self.pos[0] < x else -self.velocidade
+        self.pos[1] += self.velocidade if self.pos[1] < y else -self.velocidade
 
     def cursor(self, color, pos, size):
         pygame.draw.circle(self.screen, color, pos, size)
@@ -39,43 +69,13 @@ class Jogo:
         self.ini = time()
         self.score = 0
 
-    def text(self, text, font, font_color, size, pos):
-        font = pygame.font.SysFont(font, size)
-        label = font.render(text, 1, font_color)
+    def text(self, text, pos):
+        font = pygame.font.SysFont(self.font, self.font_size)
+        label = font.render(text, 1, self.text_color)
         self.screen.blit(label, pos)
 
     def run(self):
-        pygame.init()
-        tk = Tk()
-        w = tk.winfo_screenwidth()
-        h = tk.winfo_screenheight()
-        self.width = w * 0.7
-        self.height = h * 0.7
-        self.background = (0, 0, 0)
-
-        self.screen = pygame.display.set_mode((self.width, self.height))
-        pygame.display.set_caption("Fuja Da Bolinha!")
-        self.screen.fill(self.background)
-
-        self.pos = [20, 20]
-        self.color = (0, 0, 150)
-        self.velo = int(self.height/100) + 3
-        pygame.draw.circle(self.screen, self.color, self.pos, 10)
-
-        self.start = False
-        self.perdeu = False
-        
-        text_color = [255-i for i in self.background]
-        font = "monospace"
-        font_size = 30
-
-        self.score = 1
-
         while True:
-
-            if self.score%500 == 0:
-                self.velo += 1
-   
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -92,37 +92,36 @@ class Jogo:
                         self.start = True
                     self.mouse_pos = pygame.mouse.get_pos()
 
-            if self.start:
-                if not self.inRange(self.pos, self.mouse_pos, self.velo): 
-                    self.move(*self.mouse_pos)
-                    self.cursor((0, 100, 0), self.mouse_pos, 8)
+            self.update_game()
 
-                else:
-                    pygame.display.update()
-                    self.perdeu = True
+    def update_game(self):
+        if self.score % 500 == 0:
+            self.velocidade += 1
+
+        if self.start:
+            if not self.inRange(self.pos, self.mouse_pos, self.velocidade): 
+                self.move(*self.mouse_pos)
+                self.cursor((0, 100, 0), self.mouse_pos, 8)
+            else:
+                pygame.display.update()
+                self.perdeu = True
+
+        if self.perdeu:
+            open("record.txt", "w").write(str(self.record))
+            self.move(*self.pos)
+            self.cursor((100, 0, 0), self.pos, 8)
+            self.text("YOU LOST!!!", (4, self.height/2))
+            self.text("R to restart", (4, self.height/2 + self.font_size))
                     
-                if self.perdeu:
-                    open("record.txt", "w").write(str(self.record))
-                    self.move(*self.pos)
-                    self.cursor((100, 0, 0), self.pos, 8)
-                    self.text(f"YOU LOST!!!", font, 
-                        (200, 200, 0), font_size, (4, self.height/2))
-
-                    self.text(f"R to restart", font, 
-                        text_color, font_size, (4, self.height/2 + font_size))
-                    
-            if not self.perdeu and self.start:
-                self.score += 0.5
+        if not self.perdeu and self.start:
+            self.score += 0.5
             
-            if self.record < self.score:
-                self.record = int(self.score)
+        if self.record < self.score:
+            self.record = int(self.score)
 
-            self.text(f"score: {self.score:.0f}", font, 
-                        text_color, font_size, (2, self.height-font_size))
-            
-            self.text(f"record: {self.record}", font, 
-                        text_color, font_size, (self.width/2, self.height-font_size))
-            pygame.display.update()
+        self.text(f"score: {self.score:.0f}", (2, self.height-self.font_size))
+        self.text(f"record: {self.record}", (self.width/2, self.height-self.font_size))
+        pygame.display.update()
 
 if __name__ == "__main__":
     Jogo().run()
